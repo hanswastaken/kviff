36e902f0cd49c6ea22966990cba1060899cb4810
asuka-laptop <>
2024-06-20 15:57:25 +0200

    bilingual logic

--- kviff-program.py
+++ kviff-program.py
@@ -9,14 +9,21 @@ from datetime import datetime, timedelta
 
 try:
     with open('MujProgram.txt', 'r', encoding='utf-8') as file:
-        data = file.read()
+        data = file.read()    
+    lang='cs'
 except:
     with open('MyProgramme.txt', 'r', encoding='utf-8') as file:
         data = file.read()
+    lang='en'
 
-# Extract films and their projection times
-film_pattern = re.compile(r'(.+?)\nRežie:.+?(\d{1,4}min.)\n.+?Projekce: (.+?)(?=\n\w|$)', re.DOTALL)
-projections_pattern = re.compile(r'(\d{1,2}\.\d{1,2}\. \d{1,2}:\d{2})')
+# Extract films and their projection times based on inputted language
+if lang == 'cs':
+    film_pattern = re.compile(r'(.+?)\nRežie:.+?(\d{1,4}min.)\n.+?Projekce: (.+?)(?=\n\w|$)', re.DOTALL)
+    projections_pattern = re.compile(r'(\d{1,2}\.\d{1,2}\. \d{1,2}:\d{2})')
+else:
+    film_pattern = re.compile(r'(.+?)\nDirector:.+?(\d{1,4}min.)\n.+?Screenings: (.+?)(?=\n\w|$)', re.DOTALL)
+    projections_pattern = re.compile(r'(\d{1,2}\/\d{1,2} \d{1,2}:\d{2})')
+
 length_pattern = re.compile(r'(\d+)min.')
 
 films = film_pattern.findall(data)
@@ -26,7 +33,10 @@ for film, length, projections in films:
     length_minutes = int(length_pattern.search(length).group(1))
     projection_times = projections_pattern.findall(projections)
     for projection in projection_times:
-        start_time = datetime.strptime(projection, '%d.%m. %H:%M')
+        if lang == 'cs':
+            start_time = datetime.strptime(projection, '%d.%m. %H:%M')
+        else:
+            start_time = datetime.strptime(projection, '%d/%m %H:%M')
         end_time = start_time + timedelta(minutes=length_minutes)
         film_projections.append((film.strip(), start_time, end_time))
 
@@ -51,7 +61,10 @@ unseen_films -= seen_films
 # Print the selected projections
 print("Films that will be possible to see:")
 for film, start_time, end_time in max_films:
-    print(f"{film}: {start_time.strftime('%d.%m. %H:%M')} - {end_time.strftime('%d.%m. %H:%M')}")
+    if lang == 'cs':
+        print(f"{film}: {start_time.strftime('%d.%m. %H:%M')} - {end_time.strftime('%d.%m. %H:%M')}")
+    else:
+        print(f"{film}: {start_time.strftime('%d.%m. %H:%M')} - {end_time.strftime('%d/%m %H:%M')}")
 
 print(f'\nIn total: {len(max_films)}')
 
